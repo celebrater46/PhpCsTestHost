de6f2ab0817ff8699c2c5f43419736951ca373e1
hideru <jota.16.stigma.world@gmail.com>
2021-06-15 21:29:23 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+
+include "class.php";
+$main = new _main_;
+if(isset($_POST['type'])) $Type = $_POST['type'];
+if(isset($_POST['data'])) $Data = $_POST['data'];
+
+if(isset($Type))
+{
+    switch (strip_tags($Type))
+    {
+        case "t_insert_data": $main->InsertData($Data); break;
+        case "t_get_data": $main->GetData(); break;
+    }
+}
